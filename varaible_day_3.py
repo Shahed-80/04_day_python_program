0f31920f03f74643fce2f5a025149bf042bad0c0
Shahed-80 <107167352+Shahed-80@users.noreply.github.com>
2022-06-14 10:50:02 +0530

    Add files via upload

---
+++ varaible_day_3.py
@@ -0,0 +1,124 @@
+''' A varaible can start with the 
+            Numeric, Alphabet, Underscore '''
+
+""" In python there is no limit for naming the variable
+           And  in c language we have a limit of 31 characters."""
+
+# Inyfgrt = "I want to decode the programme"
+# print(Inyfgrt)
+
+# ABDG = 1542652
+# print(ABDG)
+
+# a = 'Programmer'
+# print("I am " + a)
+
+# print("I want to create a programme"\
+#      + ' to decode the solution')
+
+# _python123 = 65
+# print(_python123)
+
+
+# SDf = 'Iron man'
+# print("I want a software to build  " + SDf)
+
+# _145abd = "python Classes"
+# print(_145abd)
+
+# Bc = "python programme"
+# CD = "To develop a "
+
+# print(CD + Bc)
+
+# print("Wm".isidentifier())
+
+# a_123 = 'Python'
+# print(a_123.isidentifier())
+
+# WEW = "PyThon.12"
+
+# print(WEW.lower())
+
+# WEW = "PyThon.12"
+# print(WEW.upper())
+
+# It_mn   = 'variable.123'
+
+# print(It_mn.isidentifier())
+
+
+# txt = "     Hello world        "
+# print(txt)
+
+# x = txt.strip()
+
+# print(x.isidentifier())
+# print(x)
+# print(len(x))
+
+'''Converting the string into upper case/ lower case'''
+
+# nor = "Home town"
+# print(nor.upper())
+# print(nor.lower())
+
+# wex = "Programming world"
+
+# wex = wex.replace(("P"),("L"))
+
+# print(wex)
+
+
+# age = 36
+
+# txt = "My name is John, and I am {}"
+
+# print(txt.format(age))
+
+
+# John = "My age is {}"
+# age = 45
+
+# print(John.format(age))
+
+
+# alex = "Converting my age as of now and it will be as {}"
+
+# configure = 56
+
+# print(alex .format(configure))
+
+
+a = 5
+b = 7
+b,a = a,b
+print(a)
+print(b)
+
+
+print('.'.isidentifier())
+
+a = "index"
+b = "important"
+
+a,b = b,a
+print(a)
+print(b)
+
+
+index = "I want to be a python programmer"
+output = "AI"
+
+x = index
+t = output
+
+x,t = t,x
+print(x)
+print(t)
+
+if (5>8):
+  print("5 is greater than 8")
+else:
+    print("8 is the  greatest")
+
